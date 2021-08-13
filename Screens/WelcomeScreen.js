b794c32a7e40c2a783852e6b9ec31bb79c2c6d38
poyanpayandeh1 <poyanpayandeh@gmail.com>
2021-08-13 16:11:24 +1200

    prototypes the welcome screen, as well added a screens foler.

--- Screens/WelcomeScreen.js
+++ Screens/WelcomeScreen.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
 export default function WelcomeScreen() {
+  const hi = () => {
+    alert("hi");
+  }
   return (
     <View style={styles.container}>
-      <Text>Vinson created this repository.</Text>
-      <StatusBar style="auto" />
+      <View style={styles.headerBar}></View>
+      <Text style= {styles.header}>Welcome to Patient App</Text>
+
+      <TouchableHighlight onPress={hi} style={styles.buttonSignIn}>
+        <View>
+          <Text style={styles.buttonText}>Sign In</Text>
+        </View>
+      </TouchableHighlight>
+
+      <Text style={styles.or}>Or</Text>
+
+      <TouchableHighlight onPress={hi} style={styles.buttonSignUp}>
+        <View>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </View>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -14,8 +31,52 @@ export default function WelcomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EFF1F8',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  headerBar: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#69718A',
+  },
+
+  header: {
+    fontWeight: 'bold',
+    color: '#575A68',
+    top: 200,
+    fontSize: 18,
+  },
+
+  buttonSignIn: {
+    borderRadius: 20,
+    backgroundColor: '#69718A',
+    width: 380,
+    height: 43,
+    padding: 10,
+    top: 300,
+    alignItems: 'center',
+   },
+
+   buttonSignUp: {
+    borderRadius: 20,
+    backgroundColor: '#69718A',
+    width: 380,
+    height: 43,
+    padding: 10,
+    top: 350,
+    alignItems: 'center',
+   },
+
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 14,
+  },
+
+  or: {
+    fontSize: 14,
+    color: '#C6C8D2',
+    top: 325,
+
+  }
 });

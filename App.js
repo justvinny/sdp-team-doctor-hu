96da457025762fb6a422c4abdf8193ca8372ae02
Vinson Beduya <vinsonemb.151994@gmail.com>
2021-08-16 22:09:20 +1200

    Added method to get a user by id.

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import authService from './firebase/authService';
+import firestoreService from './firebase/firestoreService';
 
 export default function App() {
-  
+  const click = () => {
+    firestoreService.getUserById("hcXH0dAlb94Hnh83R1DH")
+      .then(data => console.log(data));
+  }
+
   return (
     <View style={styles.container}>
       <Text>Vinson created this repository.</Text>
-      <Button onPress={authService.signIn("test@test.co.nz", "123456")} title="Sign-up" />
+      <Button onPress={click} title="Sign-up" />
       <StatusBar style="auto" />
     </View>
   );

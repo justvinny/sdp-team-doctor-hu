ce99f767ef8cbcf1d7e486595c2dff82c03f43d3
Vinson Beduya <vinsonemb.151994@gmail.com>
2021-08-16 20:47:17 +1200

    Added getFormattedDayNow method which gives the day today. e.g. Monday, Tuesday, etc.

--- utilities/dateUtility.js
+++ utilities/dateUtility.js
@@ -7,8 +7,30 @@ const getFormattedDateNow = () => {
         + date.getMinutes()
 }
 
+const getFormattedDayNow = () => {
+    let date = new Date();
+
+    switch (date.getDay()) {
+        case 0:
+            return "Sunday"
+        case 1:
+            return "Monday"
+        case 2:
+            return "Tuesday"
+        case 3:
+            return "Wednesday"
+        case 4:
+            return "Thursday"
+        case 5:
+            return "Friday"
+        case 6:
+            return "Saturday"
+    }
+}
+
 const dateUtility = {
-    getFormattedDateNow
+    getFormattedDateNow,
+    getFormattedDayNow
 }
 
 export default dateUtility;

107194cad03307fca6e23556d36656cf0feb10ac
Jada <jadatu13@outlook.com>
2021-08-17 15:17:37 +1200

    Added function view for profile information
    This is a reusable component that can be recalled for profile text inputs.

--- Screens/StaffProfile.js
+++ Screens/StaffProfile.js
@@ -1,13 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+function ProfileInformation({label, placeholder, value, onChangeText})
+{
+  return (
+    <View style={styles.inputView}>  
+      <Text style={styles.text}>{label}</Text>
+      <TextInput
+        value={value}
+        placeholder={placeholder}
+        onChangeText={onChangeText}
+        style={styles.input}
+      />
+    </View>
+  );
+}
 
 export default function StaffProfile() {
-    return (
-      <View style={styles.container}>
-        <Text>Jacob created this profile.</Text>
-        <StatusBar style="auto" />
-      </View>
+  const[firstName, setFirstName] = useState('');
+  const[middleName, setMiddleName] = useState('');
+  const[lastName, setLastName] = useState('');
+  return (
+    <SafeAreaProvider>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <ProfileInformation
+          label="First Name:"
+          placeholder="First Name" 
+          value={firstName}
+          onChangeText={setFirstName}/>
+          <ProfileInformation
+          label="Middle Name:"
+          placeholder="Middle Name"
+          value={middleName}
+          onChangeText={setMiddleName}/>
+          <ProfileInformation
+          label="Last Name:"
+          placeholder="Last Name"
+          value={lastName}
+          onChangeText={setLastName}/>
+        </View>
+    </SafeAreaProvider>
     );
   }
   
@@ -18,4 +53,17 @@ export default function StaffProfile() {
       alignItems: 'center',
       justifyContent: 'center',
     },
+    input: {
+      borderColor: 'black',
+      borderWidth: 0.5,
+      padding: 5,
+    },
+    text: {
+      marginRight: 10,
+    },
+    inputView: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
+    }
   });

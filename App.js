b7a4f4d340524986128a5def39800724ad233116
Vinson Beduya <vinsonemb.151994@gmail.com>
2021-08-17 13:45:56 +1200

    Added react-navigation to the project. Also did a minor fix for ignore log setTimer to only trigger when on Android as issue only arises on android.

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import authService from './firebase/authService';
-import firestoreService from './firebase/firestoreService';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const click = () => {
-    authService.signUp("test2@test2.co.nz", "123456", "Jada", "Gay", "Dude", false)
-      .then((msg) => alert(msg));
+  // Temporary to test navigation
+  const Home = ({ navigation }) => {
+    const click = () => {
+      navigation.navigate("Page")
+    }
+
+    return (
+      <View style={styles.container} >
+        <Text>Vinson created this repository.</Text>
+        <Button onPress={click} title="Sign-up" />
+      </View >
+    )
   }
 
-  return (
+  // Temporary to test navigation
+  const Page = () => (
     <View style={styles.container}>
-      <Text>Vinson created this repository.</Text>
-      <Button onPress={click} title="Sign-up" />
-      <StatusBar style="auto" />
+      <Text>Another Page.</Text>
     </View>
+  )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "orange",
+          }
+        }}
+      >
+        <Stack.Screen component={Home} name="Home" />
+        <Stack.Screen component={Page} name="Page" />
+      </Stack.Navigator>
+      <StatusBar style="light" />
+    </NavigationContainer>
+
   );
 }
 
--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,22 +1,21 @@
 import firebase from 'firebase/app'
 import "firebase/auth";
 import "firebase/firestore";
-import { LogBox } from "react-native";
+import { LogBox, Platform } from "react-native";
 
-// Ignore setting a timer warning. 
-// Comment out this line when using the expo web as it crashes the app.
-// Works fine as is though when using expo android/IOS.
-LogBox.ignoreLogs(['Setting a timer']);
+// Ignore setting a timer warning on android.
+if (Platform.OS === "android") LogBox.ignoreLogs(['Setting a timer']);
+
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyDtb_V0qVKuyrnPf60GGPrfDoduHmvd3kU",
-    authDomain: "sdp-team-doctor-hu.firebaseapp.com",
-    projectId: "sdp-team-doctor-hu",
-    storageBucket: "sdp-team-doctor-hu.appspot.com",
-    messagingSenderId: "293085614889",
-    appId: "1:293085614889:web:8df7ce008386c21d7f9d38"
-  };
+  apiKey: "AIzaSyDtb_V0qVKuyrnPf60GGPrfDoduHmvd3kU",
+  authDomain: "sdp-team-doctor-hu.firebaseapp.com",
+  projectId: "sdp-team-doctor-hu",
+  storageBucket: "sdp-team-doctor-hu.appspot.com",
+  messagingSenderId: "293085614889",
+  appId: "1:293085614889:web:8df7ce008386c21d7f9d38"
+};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);

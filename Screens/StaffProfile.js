35a03385f7620c4a00d655fc566c9ddde9ca42ab
Jada <pgr7367@autuni.ac.nz>
2021-08-19 15:52:23 +1200

    Used Visual Studio Code to format the whole document.

--- Screens/StaffProfile.js
+++ Screens/StaffProfile.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
-function ProfileInformation({label, placeholder, value, onChangeText, editable})
-{
+function ProfileInformation({ label, placeholder, value, onChangeText, editable }) {
   return (
-    <View style={styles.inputView}>  
+    <View style={styles.inputView}>
       <Text style={styles.text}>{label}</Text>
       <TextInput
         value={value}
@@ -20,21 +19,21 @@ function ProfileInformation({label, placeholder, value, onChangeText, editable})
 }
 
 export default function StaffProfile() {
-  const[firstName, setFirstName] = useState('');
-  const[middleName, setMiddleName] = useState('');
-  const[lastName, setLastName] = useState('');
-  const[enabled, setEnabled] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [middleName, setMiddleName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [enabled, setEnabled] = useState(false);
 
   const doneIcon = (
     <View style={styles.icon}>
-      <MaterialIcons name="done" size={24} color="black" style={{ marginRight: 5 }}/>
+      <MaterialIcons name="done" size={24} color="black" style={{ marginRight: 5 }} />
       <Text>Done</Text>
     </View>
   );
 
   const editIcon = (
     <View style={styles.icon}>
-      <MaterialIcons name="edit" size={24} color="black" style={{ marginRight: 5 }}/>
+      <MaterialIcons name="edit" size={24} color="black" style={{ marginRight: 5 }} />
       <Text>Edit</Text>
     </View>
   );
@@ -43,91 +42,92 @@ export default function StaffProfile() {
     enabled ? setEnabled(false) : setEnabled(true);
   };
 
- const ProfileName = (
-  <View style={styles.name}>
-    <Text>{firstName + " " + middleName + " " + lastName}</Text>
-  </View>
- );
+  const ProfileName = (
+    <View style={styles.name}>
+      <Text>{firstName + " " + lastName}</Text>
+    </View>
+  );
+
   return (
-        <View style={styles.container}>
-          <StatusBar style="auto" />
+    <View style={styles.container}>
+      <StatusBar style="auto" />
 
-          {ProfileName}
+      {ProfileName}
 
-          <View style={styles.profiles}>
-            <ProfileInformation
-            label="First Name:"
-            placeholder="First Name"
-            value={firstName}
-            onChangeText={setFirstName}
-            editable={enabled}/>
-            <ProfileInformation
-            label="Middle Name:"
-            placeholder="Middle Name"
-            value={middleName}
-            onChangeText={setMiddleName}
-            editable={enabled}/>
-            <ProfileInformation
-            label="Last Name:"
-            placeholder="Last Name"
-            value={lastName}
-            onChangeText={setLastName}
-            editable={enabled}/>
-          </View>
+      <View style={styles.profiles}>
+        <ProfileInformation
+          label="First Name:"
+          placeholder="First Name"
+          value={firstName}
+          onChangeText={setFirstName}
+          editable={enabled} />
+        <ProfileInformation
+          label="Middle Name:"
+          placeholder="Middle Name"
+          value={middleName}
+          onChangeText={setMiddleName}
+          editable={enabled} />
+        <ProfileInformation
+          label="Last Name:"
+          placeholder="Last Name"
+          value={lastName}
+          onChangeText={setLastName}
+          editable={enabled} />
+      </View>
 
-          <View>
-            <TouchableOpacity style={styles.button} onPress={editText}>
-              <Text>{enabled ? doneIcon : editIcon}</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-    );
+      <View>
+        <TouchableOpacity style={styles.button} onPress={editText}>
+          <Text>{enabled ? doneIcon : editIcon}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#eef1fa',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  icon: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profiles: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 0.5,
+    padding: 5,
+    width: 200,
+    height: 40,
+  },
+  text: {
+    marginRight: 10,
+    textAlign: 'right',
+    width: 120,
+  },
+  inputView: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 5,
+    backgroundColor: '#d3d3d3',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#d3d3d3',
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 80,
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      backgroundColor: '#eef1fa',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      padding: 10,
-    },
-    icon: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    profiles: {
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'center',
-      borderRadius: 10,
-    },
-    input: {
-      borderColor: 'black',
-      borderWidth: 0.5,
-      padding: 5,
-      width: 200,
-      height: 40,
-    },
-    text: {
-      marginRight: 10,
-      textAlign: 'right',
-      width: 120,
-    },
-    inputView: {
-      padding: 10,
-      flexDirection: 'row',
-      alignItems: 'center',
-      margin: 5,
-      backgroundColor: '#d3d3d3',
-    },
-    button: {
-      alignItems: 'center',
-      backgroundColor: '#d3d3d3',
-      padding: 10,
-      borderRadius: 10,
-      marginBottom: 80,
-    }
-  });
+});

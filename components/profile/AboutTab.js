0dd904730d4cfa308d6fe0551993cead5592712f
Jada <jadatu13@outlook.com>
2021-08-21 23:44:21 +1200

    Added Text Input & Button Functionality.
    Much the same as profile page, except one big text input with an edit button so the user knows if they can edit it or not.

--- components/profile/AboutTab.js
+++ components/profile/AboutTab.js
@@ -1,10 +1,72 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import colorDefaults from "../../theme/colorDefaults";
 
 function AboutTab() {
+  const [enabled, setEnabled] = useState(false);
+
+  function editText() {
+    /* if (enabled) {
+          save();
+          let updatedUser = {
+            ...user,
+            name: {
+              first: firstName,
+              middle: middleName,
+              last: lastName,
+            },
+          };
+          setUser(updatedUser);
+        } */
+
+    enabled ? setEnabled(false) : setEnabled(true);
+  }
+
+  const doneIcon = (
+    <View style={styles.icon}>
+      <MaterialIcons
+        name="done"
+        size={24}
+        color="black"
+        style={{ marginRight: 5 }}
+      />
+      <Text>Done</Text>
+    </View>
+  );
+
+  const editIcon = (
+    <View style={styles.icon}>
+      <MaterialIcons
+        name="edit"
+        size={24}
+        color="black"
+        style={{ marginRight: 5 }}
+      />
+      <Text>Edit</Text>
+    </View>
+  );
+
   return (
-    <View style={styles.contianer}>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        multiline
+        numberOfLines={4}
+        editable={enabled}
+      >
+        Hello World
+      </TextInput>
+      <TouchableOpacity style={styles.button} onPress={editText}>
+        <Text>{enabled ? doneIcon : editIcon}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,8 +74,28 @@ function AboutTab() {
 export default AboutTab;
 
 const styles = StyleSheet.create({
-  contianer: {
+  container: {
     flex: 1,
+    flexDirection: "column",
     padding: 10,
+  },
+  input: {
+    minHeight: 100,
+    maxHeight: 150,
+    padding: 5,
+    backgroundColor: colorDefaults.bottomBorderColor,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: colorDefaults.bottomBorderColor,
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+    alignSelf: "center",
+  },
+  icon: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });

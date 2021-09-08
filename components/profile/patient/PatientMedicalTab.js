da54a112fabf95ff85017d06af6a97b5471be317
Jada <pgr7367@autuni.ac.nz>
2021-09-08 22:20:17 +1200

    Styled all components & added KeyboardAvoidingView
    Styled dropdown for BloodType to match all other styles.
    Added Keyboard avoiding view to allow the user to type in other fields.
    
    Updated Default Button Text for dropdown

--- components/profile/patient/PatientMedicalTab.js
+++ components/profile/patient/PatientMedicalTab.js
@@ -1,33 +1,48 @@
 import React from "react";
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+} from "react-native";
 import ProfileInformation from "../ProfileInformation";
 import SelectDropdown from "react-native-select-dropdown";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import colorDefaults from "../../../theme/colorDefaults";
 
 const PatientMedicalTab = () => {
   const bloodTypes = ["A+", "O+", "B+", "AB+", "A-", "O-", "B-", "AB-"];
 
   return (
-    <View style={styles.container}>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-        }}
-      >
-        <Text style={styles.label}>Blood Type</Text>
-        <SelectDropdown
-          data={bloodTypes}
-          onSelect={(selectedItem, index) => {
-            console.log(selectedItem, index);
-          }}
-        />
-      </View>
-      <ProfileInformation label="Birthdate" />
-      <ProfileInformation label="Weight" />
-      <ProfileInformation label="Height" />
-      <ProfileInformation label="Allergies" />
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={360}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView bounces={false} style={{ flex: 1 }}>
+          <View style={styles.dropdownContainer}>
+            <Text style={styles.label}>Blood Type</Text>
+            <SelectDropdown
+              data={bloodTypes}
+              onSelect={(selectedItem, index) => {
+                console.log(selectedItem, index);
+              }}
+              buttonStyle={styles.dropdownButton}
+              defaultButtonText="None Selected"
+            />
+          </View>
+
+          <ProfileInformation label="Birthdate" />
+          <ProfileInformation label="Weight" />
+          <ProfileInformation label="Height" />
+          <ProfileInformation label="Allergies" />
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -43,6 +58,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   label: {
-    paddingRight: 15,
+    marginRight: 10,
+    textAlign: "right",
+    width: 120,
+  },
+  dropdownContainer: {
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    margin: 5,
+    backgroundColor: colorDefaults.bottomBorderColor,
+    borderRadius: 10,
+  },
+  dropdownButton: {
+    width: 200,
+    height: 40,
+    borderWidth: 0.5,
+    borderColor: "black",
+    padding: 5,
+    backgroundColor: colorDefaults.bottomBorderColor,
   },
 });

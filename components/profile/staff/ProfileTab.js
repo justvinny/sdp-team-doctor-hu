76b44d6fb37b98a73c86cc310c5c8ec2ab349368
Jacob Tupe <jadatu13@outlook.com>
2021-10-03 00:30:14 +1300

    Moved Staff Profile from React Navigaiton to Elements Tabs
    This allows the whole screen to be a keyboard avoiding view, fixing issues across multiple devices and screen sizes.
    - Created TabStyles.js stylesheet so that the styles used for the tabs can be accessed globally throughout the application.
    -Modified AboutTab.js to follow these new design changes.
    -Modified ProfileTab.js to follow these new design changes.

--- components/profile/staff/ProfileTab.js
+++ components/profile/staff/ProfileTab.js
@@ -1,15 +1,7 @@
 import React, { useContext, useState } from "react";
-import {
-  KeyboardAvoidingView,
-  StyleSheet,
-  TouchableWithoutFeedback,
-  Keyboard,
-  View,
-  Platform,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
 import ProfileInformation from "../profilecomponents/ProfileInformation";
 import firestoreService from "../../../firebase/firestoreService";
-import { ScrollView } from "react-native-gesture-handler";
 import AuthContext from "../../../context/AuthContext";
 import EditEnableButton from "../profilecomponents/EditEnableButton";
 
@@ -42,48 +34,40 @@ function ProfileTab({ user, setUser }) {
   }
 
   return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      keyboardVerticalOffset={370}
-    >
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <ScrollView style={{ flex: 1 }} bounces={false}>
-          <View style={styles.profiles}>
-            <ProfileInformation
-              label="First Name:"
-              placeholder="First Name"
-              value={firstName}
-              onChangeText={setFirstName}
-              editable={enabled}
-            />
-            <ProfileInformation
-              label="Middle Name:"
-              placeholder="Middle Name"
-              value={middleName}
-              onChangeText={setMiddleName}
-              editable={enabled}
-            />
-            <ProfileInformation
-              label="Last Name:"
-              placeholder="Last Name"
-              value={lastName}
-              onChangeText={setLastName}
-              editable={enabled}
-            />
-            {authUserId === user.id ? (
-              <EditEnableButton
-                editable={enabled}
-                setEditable={setEnabled}
-                saveChanges={editText}
-              />
-            ) : (
-              <></>
-            )}
-          </View>
-        </ScrollView>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+    <View>
+      <View style={styles.profiles}>
+        <ProfileInformation
+          label="First Name:"
+          placeholder="First Name"
+          value={firstName}
+          onChangeText={setFirstName}
+          editable={enabled}
+        />
+        <ProfileInformation
+          label="Middle Name:"
+          placeholder="Middle Name"
+          value={middleName}
+          onChangeText={setMiddleName}
+          editable={enabled}
+        />
+        <ProfileInformation
+          label="Last Name:"
+          placeholder="Last Name"
+          value={lastName}
+          onChangeText={setLastName}
+          editable={enabled}
+        />
+        {authUserId === user.id ? (
+          <EditEnableButton
+            editable={enabled}
+            setEditable={setEnabled}
+            saveChanges={editText}
+          />
+        ) : (
+          <></>
+        )}
+      </View>
+    </View>
   );
 }
 
--- components/profile/staff/StaffProfile.js
+++ components/profile/staff/StaffProfile.js
@@ -1,15 +1,25 @@
 import React, { useState, useContext, useEffect, useLayoutEffect } from "react";
-import { StyleSheet, Image, Text, View } from "react-native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+} from "react-native";
 import colorDefaults from "../../../theme/colorDefaults";
 import ProfileTab from "./ProfileTab";
 import AuthContext from "../../../context/AuthContext";
 import firestoreService from "../../../firebase/firestoreService";
 import Staff from "../../../models/Staff";
 import AboutTab from "./AboutTab";
-const Tab = createMaterialTopTabNavigator();
+import { Tab, TabView } from "react-native-elements";
+import TabStyles from "../profilecomponents/TabStyles";
 
 export default function StaffProfile({ navigation, route }) {
+  const [index, setIndex] = useState(0);
   const passedUser = route.params?.user;
   const { authUserId } = useContext(AuthContext);
   const [user, setUser] = useState(passedUser ? passedUser : {});
@@ -35,34 +45,51 @@ export default function StaffProfile({ navigation, route }) {
     }
 
     return (
-      <>
-        <View style={styles.container}>
-          <Image
-            style={styles.image}
-            source={require("../../../assets/icon.png")}
-          />
-          <Text style={styles.name}>{Staff.getFullName(user.name)}</Text>
-        </View>
+      <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <ScrollView bounces="false" style={{ flex: 1 }}>
+            <View style={styles.container}>
+              <Image
+                style={styles.image}
+                source={require("../../../assets/icon.png")}
+              />
+              <Text style={styles.name}>{Staff.getFullName(user.name)}</Text>
+            </View>
 
-        <Tab.Navigator
-          screenOptions={{
-            tabBarLabelStyle: { color: "white" },
-            tabBarStyle: { backgroundColor: colorDefaults.primary },
-            tabBarIndicatorStyle: { backgroundColor: "black" },
-          }}
-        >
-          <Tab.Screen name="Profile">
-            {({ props }) => (
-              <ProfileTab {...props} user={user} setUser={setUser} />
-            )}
-          </Tab.Screen>
-          <Tab.Screen name="About">
-            {({ props }) => (
-              <AboutTab {...props} user={user} setUser={setUser} />
-            )}
-          </Tab.Screen>
-        </Tab.Navigator>
-      </>
+            <Tab
+              value={index}
+              onChange={setIndex}
+              indicatorStyle={TabStyles.tabIndicatorStyle}
+            >
+              <Tab.Item
+                title="profile"
+                titleStyle={TabStyles.tabText}
+                style={[
+                  index == 0 ? TabStyles.activeTab : TabStyles.inactiveTab,
+                ]}
+              />
+              <Tab.Item
+                title="about"
+                titleStyle={TabStyles.tabText}
+                style={[
+                  index == 1 ? TabStyles.activeTab : TabStyles.inactiveTab,
+                ]}
+              />
+            </Tab>
+
+            <TabView value={index} onChange={setIndex} animationType="timing">
+              <TabView.Item style={{ width: "100%" }}>
+                <ProfileTab user={user} setUser={setUser} />
+              </TabView.Item>
+              <TabView.Item style={{ width: "100%" }} animationType="timing">
+                <AboutTab user={user} setUser={setUser} />
+              </TabView.Item>
+            </TabView>
+          </ScrollView>
+        </TouchableWithoutFeedback>
+        {/* This helps Keyboard Avoiding View function properly by moving the whole display up */}
+        <View style={{ height: 100 }} />
+      </KeyboardAvoidingView>
     );
   };
 

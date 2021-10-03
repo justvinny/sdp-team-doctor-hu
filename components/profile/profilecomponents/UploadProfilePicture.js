ad68a0c9442b841443a0f04c4874b55beee2ec67
Jacob Tupe <jadatu13@outlook.com>
2021-10-04 00:25:25 +1300

    Picture now uploads to firebase.
    The picture a user chooses now uploads to the firebase database.

--- components/profile/profilecomponents/UploadProfilePicture.js
+++ components/profile/profilecomponents/UploadProfilePicture.js
@@ -1,26 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image, Alert } from "react-native";
-import { Button } from "react-native-elements";
+// import React in our code
+import React, { useEffect, useState } from "react";
+
+// import all the components we are going to use
+import {
+  StyleSheet,
+  View,
+  Text,
+  LogBox,
+  Button,
+  Image,
+  Alert,
+} from "react-native";
+
 import * as ImagePicker from "expo-image-picker";
-import { auth, storage } from "../../../firebase/firebaseConfig";
+import { storage } from "../../../firebase/firebaseConfig";
+import { NavigationContainer } from "@react-navigation/native";
 
-const UploadProfilePicture = () => {
-  const [image, setImage] = useState(null);
-  const [uploading, setUploading] = useState(false);
-
+const UploadProfilePicture = ({ navigation }) => {
+  //remove warning `useNativeDriver` was not specified
   useEffect(() => {
-    (async () => {
-      if (Platform.OS !== "web") {
-        const { status } =
-          await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== "granted") {
-          alert("Sorry we need camera roll permissions to make this work!");
-        }
-      }
-    })();
+    LogBox.ignoreLogs(["Animated: `useNativeDriver`"]);
   }, []);
 
-  const pickImage = async () => {
+  const [image, setImage] = useState("");
+
+  const imagePicker = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: false,
@@ -35,29 +39,50 @@ const UploadProfilePicture = () => {
     }
   };
 
-  const betterUpload = async () => {
+  const upload = async () => {
     if (image) {
       try {
-        const childPath = `picture/${Math.random().toString(36)}`;
+        const childPath = `profile/${Math.random().toString(36)}`;
         const response = await fetch(image);
-        const blob = await response.blob;
+        const blob = await response.blob();
         const task = await storage.ref().child(childPath).put(blob);
 
-        task.ref.getDownloadURL().then((url) => alert(url));
-      } catch (e) {
-        console.log(e);
+        //task.ref.getDownloadURL().then((url) => alert(url));
+        task.ref
+          .getDownloadURL()
+          .then(() =>
+            Alert.alert("Success!", "Profile Picture updated successfully.")
+          );
+
+        navigation.goBack();
+      } catch (error) {
+        alert(error.message);
       }
     }
   };
 
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry we need camera roll permissions to make this work!");
+        }
+      }
+    })();
+  }, []);
+
   return (
-    <View>
+    <View style={{ alignItems: "center" }}>
       <Text>Upload Profile Picture</Text>
-      <Button title="Pick a photo" onPress={pickImage} />
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      <Button title="Pick a photo" onPress={imagePicker} />
+      {image ? (
+        <Image style={{ width: 200, height: 400 }} source={{ uri: image }} />
+      ) : (
+        <></>
       )}
-      <Button title="Upload" onPress={betterUpload} />
+      <Button title="Upload" onPress={upload} />
     </View>
   );
 };

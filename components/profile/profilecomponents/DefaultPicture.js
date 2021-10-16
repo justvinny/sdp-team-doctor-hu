80e117dd57ebe08d60bf9235fc366266e2a3db18
Jacob Tupe <jadatu13@outlook.com>
2021-10-17 02:01:02 +1300

    Delete DefaultPicture.js
    This is no longer used because of how profile picture is now being utilized.

--- components/profile/profilecomponents/DefaultPicture.js
+++
@@ -1,2 +0,0 @@
-export const defaultPicture =
-  "https://firebasestorage.googleapis.com/v0/b/sdp-team-doctor-hu.appspot.com/o/profile%2Ficon.png?alt=media&token=b4ee677b-3ed3-41ab-9689-1ba237967830";
